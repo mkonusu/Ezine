b29109dbf8e9b7c97d14a82fdd39ebf1111dcd19
murali-purplelogics <murali@purplelogics.com>
2015-08-23 21:02:20 +0530

    added reading default search from json

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,39 +1,13 @@
 package controllers;
 
-import com.google.api.services.youtube.model.ResourceId;
-import com.google.api.services.youtube.model.SearchListResponse;
-import com.google.api.services.youtube.model.SearchResult;
-import com.google.api.services.youtube.model.Thumbnail;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.index;
-import youtube.Search;
-
-import java.util.List;
 
 public class Application extends Controller {
 
     public static Result index() {
 
-        SearchListResponse searchListResponse =  Search.videos(SearchController.getDefaultSearchRequest("Development"));
-        List<SearchResult> searchResults = searchListResponse.getItems();
-        for(SearchResult video : searchResults) {
-            {
-                ResourceId rId = video.getId();
-
-                // Confirm that the result represents a video. Otherwise, the
-                // item will not contain a video ID.
-                if (rId.getKind().equals("youtube#video")) {
-                    Thumbnail thumbnail = video.getSnippet().getThumbnails().getDefault();
-
-                    System.out.println(video.getSnippet().getChannelId());
-                    System.out.println(" Video Id" + rId.getVideoId());
-                    System.out.println(" Title: " + video.getSnippet().getTitle());
-                    System.out.println(" Thumbnail: " + thumbnail.getUrl());
-                    System.out.println("\n-------------------------------------------------------------\n");
-                }
-            }
-        }
         return ok(index.render("Your new application is ready."));
     }
 
--- app/controllers/SearchController.java
+++ app/controllers/SearchController.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.api.services.youtube.model.SearchListResponse;
 import com.google.gson.Gson;
 import models.SearchRequest;
@@ -10,31 +11,35 @@ import play.mvc.Result;
 import youtube.Search;
 import youtube.util.ResponseMapper;
 
+import java.io.File;
+
 
 public class SearchController extends Controller {
 
+    private static String DEFAULT_SEARCH_FILE = "public/files/default-search.json";
+
     public static Result search(String topic) {
 
-        SearchRequest searchRequest = null;
-        if( request().body() == null || request().body().asJson() == null) {
-            searchRequest = getDefaultSearchRequest(topic);
-        } else {
-            JsonNode json = request().body().asJson();
-            searchRequest = new Gson().fromJson(json.toString(), models.SearchRequest.class);
+        SearchResponse response = null;
+        try {
+            ObjectMapper om = new ObjectMapper();
+            SearchRequest searchRequest;
+            if (request().body() == null || request().body().asJson() == null) {
+                JsonNode searchReqNode = om.readTree(new File(DEFAULT_SEARCH_FILE));
+                searchRequest = om.treeToValue(searchReqNode, SearchRequest.class);
+            } else {
+                JsonNode json = request().body().asJson();
+                searchRequest = new Gson().fromJson(json.toString(), models.SearchRequest.class);
+            }
+            SearchListResponse result = Search.videos(searchRequest);
+            response = ResponseMapper.getSearchResponse(result);
+
+
+        }catch(Exception e) {
+            e.printStackTrace();
+            // response  - set empty or error code
         }
-        SearchListResponse result = Search.videos(searchRequest);
-        SearchResponse response = ResponseMapper.getSearchResponse(result);
 
         return ok(new Gson().toJson(response));
-    }
-
-
-    public static SearchRequest getDefaultSearchRequest(String topic) {
-        SearchRequest req = new SearchRequest();
-        req.recordsPerPage =10;
-        req.searchKey= topic;
-        if(topic == null)  req.searchKey= "Technology";
-
-        return req;
     }
 }
